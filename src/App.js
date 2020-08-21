1a345328a5f175b725be41d765ed2f99ad3f5d2e
MERN-DEV <lyndonprado1@gmail.com>
2020-08-21 16:42:34 +1000

    react tutorial part 46 - passing method references between components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {name: 'Max', age:28 },
+      {name: 'Manu', age:29 },
+      {name: 'Stephanie', age:26}
+    ]
+  });
+
+  const [ otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+  const switchNameHandler = (Name) => {
+    console.log('Was clicked!');
+    setPersonsState({
+      persons:[
+      {name: Name, age:28 },
+      {name: 'Manu', age:29 },
+      {name: 'Stephanie', age:27}
+      ]
+    });
+  };
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={() => switchNameHandler('Maximilian')}>Switch Name</button>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}></Person>
+        <Person 
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+          click={() => switchNameHandler('Max')}>My Hobbies: Racing</Person>
+        <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}></Person>
+      </div>
+    );
+};
 export default App;
